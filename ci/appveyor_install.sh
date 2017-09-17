fd2f523aaaa7601f5298aaa7160b39657b9d3292
oddcoder <ahmedsoliman@oddcoder.com>
2017-09-17 21:58:33 +0200

    Updating Appveyor building script

--- ci/appveyor_install.sh
+++ ci/appveyor_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-curl http://bin.rada.re/radare2-w32-1.4.0.zip > radare2-w32-1.4.0.zip
-unzip radare2-w32-1.4.0.zip
-export LIBRARY_PATH=$(pwd)/radare2-w32-1.4.0
+curl http://radare.mikelloc.com/get/1.6.0/radare2-w32-1.6.0.zip > radare2.zip
+unzip radare2.zip
+mv radare2-w32-1.7.0-git radare2
+export LIBRARY_PATH=$(pwd)/radare2
 cargo build --release --verbose
-cp radare2-w32-1.4.0/*.dll target/release/
+cp radare2/*.dll target/release/
